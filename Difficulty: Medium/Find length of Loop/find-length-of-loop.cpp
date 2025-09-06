067d00a1f54c45d59a622ce13417a2be62a4542b
Mirin Mano M <146096264+Mirinmano@users.noreply.github.com>
2025-09-06 21:20:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find length of Loop/find-length-of-loop.cpp
@@ -0,0 +1,42 @@
+/*
+class Node {
+ public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+     int lengthOfLoop(Node *head) {
+        if (head == NULL || head->next == NULL) return 0;
+
+        Node *slow = head, *fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {  
+                return countLoopLength(slow);
+            }
+        }
+        return 0; 
+    }
+
+  private:
+    int countLoopLength(Node *nodeInLoop) {
+        int count = 1;
+        Node *temp = nodeInLoop->next;
+
+        while (temp != nodeInLoop) {
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+};
